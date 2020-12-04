76e186e9e7aa253e529d55283619d2c9dbf7f947
santorog <guillaume.santoro@gmail.com>
2020-12-04 18:35:44 +0000

    Advent of code - day 4 - v1

--- src/main/scala/y2020/Main.scala
+++ src/main/scala/y2020/Main.scala
@@ -2,11 +2,12 @@ package y2020
 
 import y2020.day2.Day2
 import y2020.day3.Day3
+import y2020.day4.Day4
 
 object Main extends App {
 
   //Day1.runDay1()
   //Day2.runDay2()
-  Day3.runDay3()
+  Day4.runDay4()
 
 }
