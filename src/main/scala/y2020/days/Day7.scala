f0b20ae365b6cc4f350b2cb5326ff341c524d233
santorog <31656059+santorog@users.noreply.github.com>
2021-11-14 21:51:43 +0000

    Advent of code - day7 part 2 use seq instead of set

--- src/main/scala/y2020/days/Day7.scala
+++ src/main/scala/y2020/days/Day7.scala
@@ -12,11 +12,11 @@ object Day7 {
 
   def process(f: BufferedSource): Unit = {
     val rules = parseRulesFromFile(f)
-    //star1(rules)
+    star1(rules)
     star2(rules)
   }
 
-  def parseRulesFromFile(f: BufferedSource): Map[String, Set[(String, Int)]] = {
+  def parseRulesFromFile(f: BufferedSource): Map[String, Seq[(String, Int)]] = {
     f.mkString
       .split("\n")
       .map(rule => rule.split(" bags contain "))
@@ -24,11 +24,10 @@ object Day7 {
       .toMap
   }
 
-  def parseBagContentList(bags: String): Set[(String, Int)] = {
+  def parseBagContentList(bags: String): Seq[(String, Int)] = {
     " bag[s]?[,.][ ]?".r.split(bags)
       .filter(s => s.nonEmpty && !s.equals("no other"))
       .map(s => parseIndividualBag(s))
-      .toSet
   }
 
   def parseIndividualBag(bags: String): (String, Int) = {
@@ -37,13 +36,13 @@ object Day7 {
     (bagType, count.toInt)
   }
 
-  def star1(rules: Map[String, Set[(String, Int)]]): Unit = {
+  def star1(rules: Map[String, Seq[(String, Int)]]): Unit = {
     println(findOuterBags(rules, "shiny gold").size)
   }
 
-  def findOuterBags(rules: Map[String, Set[(String, Int)]], myBag: String): Set[String] = {
+  def findOuterBags(rules: Map[String, Seq[(String, Int)]], myBag: String): Set[String] = {
 
-    def analyzeRecursively(rules: Map[String, Set[(String, Int)]], myBag: String, acc: Set[String]): Set[String] = {
+    def analyzeRecursively(rules: Map[String, Seq[(String, Int)]], myBag: String, acc: Set[String]): Set[String] = {
 
       val outerBagCandidates = rules.filter(rule => rule._2.exists(innerBag => innerBag._1.equals(myBag)))
       if (outerBagCandidates.isEmpty) acc
@@ -56,12 +55,12 @@ object Day7 {
     analyzeRecursively(rules, myBag, Set.empty)
   }
 
-  def star2(rules: Map[String, Set[(String, Int)]]): Unit = {
+  def star2(rules: Map[String, Seq[(String, Int)]]): Unit = {
     println(findInnerBagsCount(rules, "shiny gold"))
   }
 
-  def findInnerBagsCount(rules: Map[String, Set[(String, Int)]], myBag: String): Int = {
-    val bagContent = rules.getOrElse(myBag, Set.empty)
+  def findInnerBagsCount(rules: Map[String, Seq[(String, Int)]], myBag: String): Int = {
+    val bagContent = rules.getOrElse(myBag, Seq.empty)
     bagContent.map(v => v._2 + v._2 * findInnerBagsCount(rules, v._1)).sum
   }
 
